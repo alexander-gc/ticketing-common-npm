c6cb3127c6392ec5eb60108751ba0828942e02ab
alexander-gc <alexandergcca@gmail.com>
2023-02-07 17:30:34 -0600

    some comments added 2

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-// Errors
+// Errors //
 export * from "./errors/bad-request-err";
 export * from "./errors/custom-error";
 export * from "./errors/db-connection-err";
@@ -6,7 +6,7 @@ export * from "./errors/not-authorized-err";
 export * from "./errors/not-found-err";
 export * from "./errors/req-validation-err";
 
-// Middlewares
+// Middlewares //
 export * from "./middlewares/current-user";
 export * from "./middlewares/error-handler";
 export * from "./middlewares/require-auth";
